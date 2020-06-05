b9bcea83ef5055878459d4c0cc82fb09e869c347
christinewj81 <christinewj81@hotmail.com>
2020-06-05 13:26:07 +0200

    CMake Library Step1

---
+++ examples/main.cpp
@@ -0,0 +1,7 @@
+#include <foo/Hello.h>
+
+int main()
+{
+	hello();
+	return 0;
+}
---
+++ foo/src/foo/Hello.cpp
@@ -0,0 +1,8 @@
+#include "foo/Hello.h"
+#include <iostream>
+
+void hello()
+{
+	std::cout << "Hello World, I am here!" <<std::endl;
+	return;
+}
